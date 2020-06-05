228cd9ab2675b6848e21de5f3ee304b0a3f1291b
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2020-06-05 02:09:47 +0100

    View all books in db

--- routes/books.js
+++ routes/books.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
-var books = [];
+var Book = require('../models/book')
 
 /* GET all books. */
 router.get('/', function(req, res, next) {
-  if (books.length == 0){
+
+  Book.find(function(err, books) {
+           
+    if (err) { throw err; }
+     if (books.length == 0){
     var alert = "The library is empty"
   }
-  res.render('books/index', {alert,  books: books });
+
+    res.render('books/index', { alert, books : books});
+  });
 });
 
 module.exports = router;
